bb15aad370cdf8b9d09432093b2d5ad8f6f29498
plonky <netmonk>
2015-03-02 17:02:45 +0100

    uploading library and test file

---
+++ moneronjs.js
@@ -0,0 +1,109 @@
+exports.Wallet = Wallet;
+
+var http = require('http');
+
+function jsonHttpRequest(host, port, data, callback){
+
+    var options = {
+        hostname: host,
+        port: port,
+        path: '/json_rpc',
+        method: 'POST',
+        headers: {
+            'Content-Length': data.length,
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    };
+
+    var req = http.request(options, function(res){
+        var replyData = '';
+        res.setEncoding('utf8');
+        res.on('data', function(chunk){
+            replyData += chunk;
+        });
+        res.on('end', function(){
+            var replyJson;
+            try{
+                replyJson = JSON.parse(replyData);
+            }
+            catch(e){
+                callback(e);
+                return;
+            }
+            callback(null, replyJson);
+        });
+    });
+
+    req.on('error', function(e){
+        callback(e);
+    });
+
+    req.end(data);
+}
+
+function rpc(host, port, method, params, callback){
+
+    var data = JSON.stringify({
+        id: "0",
+        jsonrpc: "2.0",
+        method: method,
+        params: params
+    });
+    jsonHttpRequest(host, port, data, function(error, replyJson){
+        if (error){
+            callback(error);
+            return;
+        }
+        callback(replyJson.error, replyJson.result);
+    });
+}
+
+
+function Wallet(ip,port){
+	var self = this; 
+	self.opt = {
+		ip: ip,
+		port: port
+	};
+	if (typeof self.opt.ip === "undefined") {self.opt.ip = "127.0.0.1"}; 
+	if (typeof self.opt.port == "undefined") {self.opt.port = "8082"}
+
+	self.getaddress = function (callback){
+		rpc(self.opt.ip, self.opt.port, "getaddress",'', function(error,result){
+                    if (error){
+	         	    console.log("error in getting address"+error);
+                            return;
+		   } else { //console.log(result)
+		   	var address = result.address;
+			callback(address); 
+		   
+		   
+		   }
+		});	
+	}
+	self.getbalance = function(callback){
+                rpc(self.opt.ip, self.opt.port, "getbalance",'', function(error,result){
+                    if (error){
+                            console.log("error in getting balance"+error);
+                            return;
+		    } else {//console.log(result)
+			    var unlocked = result.unlocked_balance;
+			    var balance = result.balance;
+			    callback(balance, unlocked);
+		    }
+		});     
+	}
+	self.getpayment= function(pid, callback){
+		rpc(self.opt.ip, self.opt.port, "get_payments", { "payment_id" : pid }, function(error,result){
+		     if (error){
+		             console.log("error in getting payment"+error);
+		             return;
+		     } else {//console.log(result)
+			     callback(result);
+		     }
+		});
+	}
+}
+
+
---
+++ test.js
@@ -0,0 +1,25 @@
+var wallet = require('./moneronjs.js');
+
+console.log("this is a wallet rpc interface test");
+var wal =  new wallet.Wallet("127.0.0.1","10500");
+console.log(wal.opt);
+wal.getaddress(function(address){
+          console.log("lets try to get the address of the wallet");
+          console.log(address);
+});
+wal.getbalance(function(bal,ulock){
+        console.log("lets try to get balance of the wallet");
+        console.log("total balance: "+ bal);
+        console.log("unlocked balance: "+ ulock);
+});
+wal.getpayment("d9ed4ef9e7d80f71f90d419152720177db3df9975f52a50f0172276224214b1b",function(result){
+        console.log("let's try to get payment by id : d9ed4ef9e7d80f71f90d419152720177db3df9975f52a50f0172276224214b1b");
+        console.log(result);
+});
+  
+  
+wal.getpayment("0000000000000000000000000000000000000000000000000000000000000000",function(result){
+          console.log("let's try to get payment by id : 0000000000000000000000000000000000000000000000000000000000000000");
+          console.log(result);
+});
+
