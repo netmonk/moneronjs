37c97b3e4677254eb292dab91fee9ffd9c92ea94
aradesh <aradeshworking@web.de>
2015-03-12 22:24:20 +0100

    jslint compliance

--- moneronjs.js
+++ moneronjs.js
@@ -1,14 +1,20 @@
-exports.Wallet = Wallet;
+/*jslint node: true */
+/*jslint nomen: true */
+/*jslint plusplus: true */
+/*jshint -W009 */
+'use strict';
 
 var assert = require('assert');
 var http = require('http');
-var crypto = require('crypto'),   
+var crypto = require('crypto'),
     algorithm = 'aes-256-ctr',
     password = 'aaaaaaaaaa';
 
-function jsonHttpRequest(host, port, data, callback){
+function jsonHttpRequest(host, port, data, callback) {
+    
+    var options, req;
 
-    var options = {
+    options = {
         hostname: host,
         port: port,
         path: '/json_rpc',
@@ -20,18 +26,17 @@ function jsonHttpRequest(host, port, data, callback){
         }
     };
 
-    var req = http.request(options, function(res){
+    req = http.request(options, function (res) {
         var replyData = '';
         res.setEncoding('utf8');
-        res.on('data', function(chunk){
+        res.on('data', function (chunk) {
             replyData += chunk;
         });
-        res.on('end', function(){
+        res.on('end', function () {
             var replyJson;
-            try{
+            try {
                 replyJson = JSON.parse(replyData);
-            }
-            catch(e){
+            } catch (e) {
                 callback(e);
                 return;
             }
@@ -39,14 +44,14 @@ function jsonHttpRequest(host, port, data, callback){
         });
     });
 
-    req.on('error', function(e){
+    req.on('error', function (e) {
         callback(e);
     });
 
     req.end(data);
 }
 
-function rpc(host, port, method, params, callback){
+function rpc(host, port, method, params, callback) {
 
     var data = JSON.stringify({
         id: "0",
@@ -54,8 +59,8 @@ function rpc(host, port, method, params, callback){
         method: method,
         params: params
     });
-    jsonHttpRequest(host, port, data, function(error, replyJson){
-        if (error){
+    jsonHttpRequest(host, port, data, function (error, replyJson) {
+        if (error) {
             callback(error);
             return;
         }
@@ -63,137 +68,139 @@ function rpc(host, port, method, params, callback){
     });
 }
 
-function zeroFill( number, width )
-{
-  width -= number.toString().length;
-  if ( width > 0 )
-  {
-        return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
-      }
-  return number + ""; // always return a string
+function zeroFill(number, width) {
+    width -= number.toString().length;
+    if (width > 0) {
+        return new Array(width + (/\./.test(number) ? 2 : 1)).join('0') + number;
+    }
+    return String(number); // always return a string
+}
+
+function generatecypheredpaymentid(uid, amount, pass, callback) {
+    assert.equal(typeof (amount), 'number', 'argument amount must be a number');
+    assert.ok(!isNaN(amount) && amount > 0 && amount < 1000000000, "argument amount must be integer between 0 and 999999999");
+
+    var error, toocrypt, cipher, txt, crypted;
+    
+    if (amount > 999999999) {
+        error = 'amount too big';
+        callback(error, null);
+        return;
+    } else if (uid.length > 10) {
+        error = 'uid lenght to big';
+        callback(error, null);
+        return;
+    } else if (pass.length < 6) {
+        error = 'pass too weak, choose a longer pass';
+        callback(error, null);
+        return;
+    }
+    toocrypt = [Date.now(), uid, zeroFill(amount, 9)];
+    crypto.password = pass;
+    cipher = crypto.createCipher('aes-256-ctr', pass);
+    //console.log("to encrypt: "+toocrypt.join(''));
+    txt = toocrypt.join('');
+    crypted = cipher.update(txt, 'utf8', 'hex');
+    crypted += cipher.final('hex');
+    //console.log(crypted);
+    return callback(null, crypted);
+}
+
+function decyphercryptedpaymentid(pid, pass, callback) {
+    var decipher, dec;
+    
+    crypto.password = pass;
+    decipher = crypto.createDecipher('aes-256-ctr', pass);
+    dec = decipher.update(pid, 'hex', 'utf8');
+    dec += decipher.final('utf8');
+    console.log(dec);
+    return callback(null, dec);
 }
 
 
-function generatecypheredpaymentid(uid,amount,pass,callback){
-	  assert.equal(typeof(amount),'number', "argument amount must be a number");
-	  assert.ok(!isNaN(amount) && amount >0 && amount < 1000000000, "argument amount must be integer between 0 and 999999999");
-
-          if (amount > 999999999) {
-                  error="amount too big";
-                  callback(error,null);
-                  return;
-          } else if (uid.length > 10){
-                  error="uid lenght to big";
-                  callback(error,null);
-                  return;
-          } else if (pass.length < 6 ) {
-                  error="pass too weak, choose a longer pass";
-                  callback(error,null);
-                  return;
-          }
-          var toocrypt = [Date.now(), uid,zeroFill(amount,9)];
-          crypto.password= pass;
-          var cipher =  crypto.createCipher("aes-256-ctr",pass);
-          //console.log("to encrypt: "+toocrypt.join(''));
-          var txt=toocrypt.join("");
-          var crypted = cipher.update(txt,'utf8', 'hex');
-          crypted += cipher.final('hex');
-          //console.log(crypted);
-          return callback(null,crypted);
-}
-
-function decyphercryptedpaymentid(pid,pass,callback){
-        crypto.password = pass;
-        var decipher = crypto.createDecipher("aes-256-ctr", pass);
-        var dec = decipher.update(pid,'hex','utf8');
-        dec += decipher.final('utf8');
-        console.log(dec);
-        return callback(null,dec);
-
-}
-
-
-function Wallet(ip,port){
-	var self = this; 
+function Wallet(ip, port) {
+	var self = this;
 	self.opt = {
 		ip: ip,
 		port: port,
-		openalias:''
+		openalias: ''
 	};
-	if (typeof self.opt.ip === "undefined") {self.opt.ip = "127.0.0.1"}; 
-	if (typeof self.opt.port == "undefined") {self.opt.port = "8082"}
+	if (typeof self.opt.ip === "undefined") { self.opt.ip = '127.0.0.1'; }
+	if (typeof self.opt.port === "undefined") { self.opt.port = '8082'; }
 
-	self.getaddress = function (callback){
-		rpc(self.opt.ip, self.opt.port, "getaddress",'', function(error,result){
-                    if (error){
-	         	    console.log("error in getting address: %j", [error]);
-                            return;
-		   } else { //console.log(result)
-		   	var address = result.address;
-			callback(address); 
-		   
-		   
-		   }
-		});	
-	}
-	self.getbalance = function(callback){
-                rpc(self.opt.ip, self.opt.port, "getbalance",'', function(error,result){
-                    if (error){
-                            console.log("error in getting balance: %j", [error]);
-                            return;
-		    } else {//console.log(result)
-			    var unlocked = result.unlocked_balance;
-			    var balance = result.balance;
+	self.getaddress = function (callback) {
+		rpc(self.opt.ip, self.opt.port, 'getaddress', '', function (error, result) {
+            if (error) {
+                console.log("error in getting address: %j", [error]);
+                return;
+		    } else {
+                //console.log(result)
+                var address = result.address;
+                callback(address);
+            }
+		});
+	};
+	self.getbalance = function (callback) {
+        rpc(self.opt.ip, self.opt.port, "getbalance", '', function (error, result) {
+            if (error) {
+                console.log("error in getting balance: %j", [error]);
+                return;
+		    } else {
+                //console.log(result)
+			    var unlocked = result.unlocked_balance,
+			        balance = result.balance;
 			    callback(balance, unlocked);
 		    }
-		});     
-	}
-	self.getpaymentfromid= function(pid, callback){
-		rpc(self.opt.ip, self.opt.port, "get_payments", { "payment_id" : pid }, function(error,result){
-		     if (error){
-		             console.log("error in getting payment: %j",[error]);
-		             return;
-		     } else {//console.log(result)
-			     callback(result);
-		     }
 		});
-	}
-        self.incoming_transfers= function(type,callback){
-                rpc(self.opt.ip, self.opt.port, "incoming_transfers", { "transfer_type" : type }, function(error,result){
-                     if (error){
-                             console.log("error in getting incoming_transfers: %j", [ error]);
-                             return;
-                     } else {
-                             callback(result);
-                     }
-                });
-        }
-	self.getcypheredpaymentid= function(id,amount,pass, callback){
-		generatecypheredpaymentid(id,amount,pass, function(error,result){
-		        if (error) {
-			                console.log(error);
-			                return;
-			        }else {
-				        //console.log("result: "+result+" "+result.length);
-				        callback(result);
-				        }
+	};
+	self.getpaymentfromid = function (pid, callback) {
+		rpc(self.opt.ip, self.opt.port, "get_payments", { "payment_id" : pid }, function (error, result) {
+            if (error) {
+                console.log("error in getting payment: %j", [error]);
+                return;
+            } else {
+                //console.log(result)
+                callback(result);
+            }
 		});
-	}
-	self.decypherpaymentid= function(pid,pass, callback){
-		decyphercryptedpaymentid(pid,pass,function(error,result){
-			if(error ) {
-			callback(error);
+	};
+    
+    self.incoming_transfers = function (type, callback) {
+        rpc(self.opt.ip, self.opt.port, "incoming_transfers", { "transfer_type" : type }, function (error, result) {
+            if (error) {
+                console.log("error in getting incoming_transfers: %j", [ error]);
+                return;
+            } else {
+                callback(result);
+            }
+        });
+    };
+	self.getcypheredpaymentid = function (id, amount, pass, callback) {
+		generatecypheredpaymentid(id, amount, pass, function (error, result) {
+            if (error) {
+                console.log(error);
+			    return;
+            } else {
+                //console.log("result: "+result+" "+result.length);
+                callback(result);
+            }
+		});
+	};
+	self.decypherpaymentid = function (pid, pass, callback) {
+		decyphercryptedpaymentid(pid, pass, function (error, result) {
+            if (error) {
+                callback(error);
 			} else {
-			callback(result);
+                callback(result);
 			}
 		});
 
-	}
-	self.getrandompaymentid= function(callback){
+	};
+	self.getrandompaymentid = function (callback) {
 		var buf = crypto.randomBytes(32);
 		return callback(buf.toString('hex'));
-	}
+	};
 
 }
 
-
+exports.Wallet = Wallet;
